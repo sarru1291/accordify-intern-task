177ef282f62f1df782b7dd54d90c4e42acfe34d8
sarru1291 <sarru1291@gmail.com>
2019-07-23 12:09:07 +0530

    material add checked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'accordify-intern-task';
+  title = 'Accordify Intern Task';
 }
